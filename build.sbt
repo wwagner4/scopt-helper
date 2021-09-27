0162add6b8c57a8b335e1f90fee70824237bfafd
wwagner4 <wwagner4@gmail.com>
2021-09-27 21:50:35 +0200

    added scopt

--- build.sbt
+++ build.sbt
@@ -1,1 +1,3 @@
 scalaVersion := "3.0.2"
+
+libraryDependencies += "com.github.scopt" %% "scopt" % "4.0.1"
--- src/main/scala/clitest/Main.scala
+++ src/main/scala/clitest/Main.scala
@@ -1,12 +1,45 @@
 package clitest
+import scopt.OParser
+
+import java.io.File
 
 object Main {
 
+  case class Config(
+                     foo: Int = -1,
+                     out: File = new File("."),
+                     xyz: Boolean = false,
+                     libName: String = "",
+                     maxCount: Int = -1,
+                     verbose: Boolean = false,
+                     debug: Boolean = false,
+                     mode: String = "",
+                     files: Seq[File] = Seq(),
+                     keepalive: Boolean = false,
+                     jars: Seq[File] = Seq(),
+                     kwargs: Map[String, String] = Map())
+
+  val builder = OParser.builder[Config]
+  val parser1 = {
+    import builder.*
+    OParser.sequence(
+      programName("cli-test"),
+      head("cli-test", "0.1"),
+      // option -f, --foo
+      opt[Int]('f', "foo")
+        .action((x, c) => c.copy(foo = x))
+        .text("foo is an integer property"),
+      // more options here...
+    )
+  }
+
   def main(args: Array[String]): Unit = {
-    if (args.isEmpty) println("no arguments")
-    else {
-      val argStr = args.mkString(", ")
-      println(s"Called main with arguments: $argStr")
+    // OParser.parse returns Option[Config]
+    OParser.parse(parser1, args, Config()) match {
+      case Some(config) =>
+        println(config)
+      case _ =>
+      // arguments are bad, error message will have been displayed
     }
   }
 
