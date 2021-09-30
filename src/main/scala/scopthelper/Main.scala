fb2785ca91a9f9cd73893d7f0b437447697a8f57
wwagner4 <wwagner4@gmail.com>
2021-09-30 20:37:26 +0200

    cooking example compiled and runnable

--- src/main/scala/scopthelper/Main.scala
+++ src/main/scala/scopthelper/Main.scala
@@ -4,64 +4,24 @@ import scopt.OParser
 
 object Main {
 
-  sealed trait Mode
-
-  case object Update extends Mode
-
-  case class Insert(num: Int) extends Mode
-
-  case class CliCfg(
-                     foo: Int = -1,
-                     libName: String = "",
-                     mode: Mode = Update
-                   )
-
-  val builder = OParser.builder[CliCfg]
-  val parser = {
-    import builder.*
-    OParser.sequence(
-      programName("cli-test"),
-      head("cli-test"),
-      opt[Int]('f', "foo")
-        .action((x, c) => c.copy(foo = x))
-        .text("foo is an integer property"),
-      opt[String]('l', name = "libName")
-        .action((x, c) => c.copy(libName = x))
-        .text("library name. string"),
-      help('h', "help")
-        .text("prints this usage text"),
-      cmd("update")
-        .action((_, c) => c.copy(mode = Update))
-        .text("     update whatever"),
-      cmd("insert")
-        .action((_, c) => c.copy(mode = Insert(0)))
-        .text("     insert is somthing to be inserted")
-        .children(
-          opt[Int]('n', "number")
-            .action((x, c) => c.mode match {
-              case i: Insert => c.copy(mode = i.copy(num = x))
-              case _ => throw IllegalStateException()
-            })
-            .text("Number of inserts"),
-        )
-    )
-  }
-
-  def main(args: Array[String]): Unit = {
-    // OParser.parse returns Option[Config]
-    OParser.parse(parser, args, CliCfg()) match {
-      case Some(config) =>
-        println(config)
-      case _ =>
-      // arguments are bad, error message will have been displayed
-    }
-  }
+  def main(args: Array[String]): Unit =
+    Cooking.parse(args)
 
   object Cooking {
 
-    case class CookungData(
-                            vagan: Boolean = false,
-                            command: Command,
+    def parse(args: Array[String]): Unit = {
+      // OParser.parse returns Option[Config]
+      OParser.parse(parser, args, CookingData()) match {
+        case Some(shoppingData) =>
+          println(shoppingData)
+        case _ =>
+          println("Something went wrong ???")
+      }
+    }
+
+    case class CookingData(
+                            vegan: Boolean = false,
+                            command: Command = Shopping(),
                           )
 
     trait Command
@@ -74,8 +34,58 @@ object Main {
     case class Prepare(
                         numberOfPersons: Int = 1,
                         motto: String = ""
-                      )
+                      ) extends Command
 
+    val builder = OParser.builder[CookingData]
+    val parser = {
+      import builder.*
+      OParser.sequence(
+        programName("cooking"),
+        head("Lets do some shopping and then prepare a delicious meal"),
+        opt[Boolean]('v', "vegan")
+          .action((x, c) => c.copy(vegan = x))
+          .text("if present the only vegan ingrediens"),
+        help('h', "help").text("prints this usage text"),
+        cmd("shopping")
+          .text("lets go shopping for our meal")
+          .action((_, c) => c.copy(command = Shopping()))
+          .children(
+            opt[Int]('n', "number-of-persons")
+              .text("Number of persons")
+              .action((x, c) => {
+                val s = c.asInstanceOf[Shopping]
+                c.copy(command = s.copy(number = x))
+              })
+            ,
+            opt[String]('d', "description")
+              .text("Describes our ingrediences")
+              .action((x, c) => {
+                val s = c.asInstanceOf[Shopping]
+                c.copy(command = s.copy(description = x))
+              })
+            ,
+          )
+        ,
+        cmd("prepare")
+          .text("lets prepare our meal")
+          .action((_, c) => c.copy(command = Prepare()))
+          .children(
+            opt[Int]('n', "number-of-persons")
+              .text("Number of persons")
+              .action((x, c) => {
+                val p = c.asInstanceOf[Prepare]
+                c.copy(command = p.copy(numberOfPersons = x))
+              })
+            ,
+            opt[String]('m', "motto")
+              .text("Motto of what we are cooking. E.g. italien, ...")
+              .action((x, c) => {
+                val p = c.asInstanceOf[Prepare]
+                c.copy(command = p.copy(motto = x))
+              })
+          )
+        ,
+      )
+    }
   }
-
 }
